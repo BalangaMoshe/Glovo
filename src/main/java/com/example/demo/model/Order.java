dcc4004e9e586088fbb1c08bb71250edb9ef8e7a
Mykola Kovalets <kolya.kovalets@ukr.net>
2024-07-03 17:38:10 +0200

    init commit

--- src/main/java/com/example/demo/model/Order.java
+++ src/main/java/com/example/demo/model/Order.java
@@ -1,28 +1,20 @@
 package com.example.demo.model;
 
+
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public class Order {
+@Data
+public class Order {                // Використання Lombok
     private UUID id;
     private List<Product> products;
 
     public Order() {
         this.id = UUID.randomUUID();
         this.products = new ArrayList<>();
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public List<Product> getProducts() {
-        return products;
-    }
-
-    public void setProducts(List<Product> products) {
-        this.products = products;
     }
 
     public void addProduct(Product product) {
