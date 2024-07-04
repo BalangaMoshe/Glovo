c1e5be177352db6e1eeda9b1c815edac7de0dd00
BalangaMoshe <31573800+BalangaMoshe@users.noreply.github.com>
2024-07-04 19:41:05 +0200

    Update OrderController.java
    комент

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
-import java.util.UUID;
+import java.util.UUID; //тут лишив я коммент зерез GitHub уже
 
 @RestController
 @RequestMapping("/orders")
